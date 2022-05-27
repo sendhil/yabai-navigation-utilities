dfea2bdd4818222cc70eda2f70116ff66d4ce92d
Sendhil Panchadsaram <sendhilp@gmail.com>
2022-05-27 13:33:09 -0700

    More refactoring

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ class Options(object):
     skip_focus: bool
 
 
+@dataclass
+class WindowDetails(object):
+    window_id: int
+    space_id: int
+
+
 def get_options() -> Options:
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--app", help="App Name")
@@ -60,31 +66,51 @@ def get_spaces_data() -> Any:
     return call_yabai(["-m", "query", "--spaces"])
 
 
-def main():
-    options = get_options()
-
-    window_id: Optional[int] = None
-    for window in get_window_data():
-        if window["app"] == options.app_name:
-            window_id = window["id"]
-
-    if window_id is None:
-        raise f"{options.app_name} not found."
-
+def find_current_space() -> int:
     space_index: Optional[int] = None
     for space in get_spaces_data():
         if space["has-focus"]:
             space_index = space["index"]
 
     if space_index is None:
-        raise "Could not find current space"
+        raise Exception("Could not find current space")
 
-    # Move window to space
-    call_yabai(["-m", "window", str(window_id), "--space", str(space_index)])
+    return space_index
+
+
+def find_app_window(app_name: str) -> WindowDetails:
+    window_details: Optional[WindowDetails] = None
+    for window in get_window_data():
+        if window["app"] == app_name:
+            window_details = WindowDetails(window_id=window["id"],
+                                           space_id=window["space"])
+
+    if window_details is None:
+        raise Exception(f"Could not find app {app_name}")
+
+    return window_details
+
+
+def move_window_to_current_space(window_details: WindowDetails):
+    call_yabai([
+        "-m", "window",
+        str(window_details.window_id), "--space",
+        str(find_current_space())
+    ])
+
+
+def focus_on_window(window_id: int):
+    call_yabai(["-m", "window", str(window_id), "--focus"])
+
+
+def main():
+    options = get_options()
+    window_details = find_app_window(options.app_name)
+    move_window_to_current_space(window_details)
 
     # Focus on window
     if not options.skip_focus:
-        call_yabai(["-m", "window", str(window_id), "--focus"])
+        focus_on_window(window_details.window_id)
 
 
 if __name__ == "__main__":
